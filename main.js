0452155d34dc230d32b640872abc2723cef16c3d
Halfgods <justinroy9702@gmail.com>
2025-03-26 21:54:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Python Dev", "FrontEnd Dev", "Student","Gamer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
